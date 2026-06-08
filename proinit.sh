5db7ea7c14ee2048b0df616e59b3d034da63d90f
yasandu0505 <yasanduyaka2005@gmail.com>
2026-06-09 00:16:36 +0530

    feat : adding logs and steps

--- proinit.sh
+++ proinit.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-setup -e
+set -e
 
 echo "Running project initialization script"
-
 
 read -p "What is the project name? " PROJECT_NAME
 read -p "What is the project description? " PROJECT_DESCRIPTION
@@ -12,19 +11,35 @@ read -p "What is the project repository url? in SSH format: " PROJECT_REPOSITORY
 read -p "What is the project local location path?  (Absolute path): " PROJECT_LOCAL_LOCATION_PATH
 
 # create the project directory
-mkdir -p "$PROJECT_LOCAL_LOCATION_PATH"
+echo "Creating your project directory....."
+PROJECT_DIR="${PROJECT_LOCAL_LOCATION_PATH%/}/${PROJECT_NAME}"
+mkdir -p "$PROJECT_DIR"
+echo "Project created at ${PROJECT_DIR}"
 
 # create a README.md file with the project name and description
-echo "# $PROJECT_NAME" > "$PROJECT_LOCAL_LOCATION_PATH/README.md"
-echo "$PROJECT_DESCRIPTION" >> "$PROJECT_LOCAL_LOCATION_PATH/README.md"
-echo "\nAuthor: $PROJECT_AUTHOR" >> "$PROJECT_LOCAL_LOCATION_PATH/README.md"
+echo "Creating README.md file with project name and description....."
+echo "
+# $PROJECT_NAME
+------------------
+$PROJECT_DESCRIPTION
+
+
+### Author $PROJECT_AUTHOR
+" > "$PROJECT_DIR/README.md"
+echo "README.md file created with project name and description at ${PROJECT_DIR}/README.md"
 
 # initialize a git repository in the project directory
-git -C "$PROJECT_LOCAL_LOCATION_PATH" init
-git -C "$PROJECT_LOCAL_LOCATION_PATH" remote add origin "$PROJECT_REPOSITORY_URL"
+echo "Initializing git repository in the project directory....."
+git -C "$PROJECT_DIR" init
+echo "Adding remote origin in the project directory at ${PROJECT_DIR}......"
+git -C "$PROJECT_DIR" remote add origin "$PROJECT_REPOSITORY_URL"
 
 # add the README.md file to the git repository
-git -C "$PROJECT_LOCAL_LOCATION_PATH" add "./README.md"
+echo "Adding README.md file to the git repository....."
+git -C "$PROJECT_DIR" add "./README.md"
 
+echo "Making the initial commit....."
 # commit the README.md file to the git repository
-git -C "$PROJECT_LOCAL_LOCATION_PATH" commit -m "Initial commit"
+git -C "$PROJECT_DIR" commit -m "Initial commit"
+
+echo "Project initialization completed successfully!"
