bd7a58b35a3c578c81dc9abd17e445773a224f06
yasandu0505 <yasanduyaka2005@gmail.com>
2026-06-08 19:38:22 +0530

    initial commit

---
+++ proinit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+setup -e
+
+echo "Running project initialization script"
+
+# take the first question as the project name
+# take the second question as the project description
+# take the third question as the project author
+# take the sixth question as the project repository url
+# take the seventh question as the project local location path
+
+read -p "What is the project name? " PROJECT_NAME
+read -p "What is the project description? " PROJECT_DESCRIPTION
+read -p "Who is the project author? " PROJECT_AUTHOR
+read -p "What is the project repository url? in SSH format: " PROJECT_REPOSITORY_URL
+read -p "What is the project local location path?  (Absolute path): " PROJECT_LOCAL_LOCATION_PATH
+
+# create the project directory
+mkdir -p "$PROJECT_LOCAL_LOCATION_PATH"
+
+# create a README.md file with the project name and description
+echo "# $PROJECT_NAME" > "$PROJECT_LOCAL_LOCATION_PATH/README.md"
+echo "## $PROJECT_DESCRIPTION" >> "$PROJECT_LOCAL_LOCATION_PATH/README.md"
+
+# initialize a git repository in the project directory
+git -C "$PROJECT_LOCAL_LOCATION_PATH" init
+git -C "$PROJECT_LOCAL_LOCATION_PATH" remote add origin "$PROJECT_REPOSITORY_URL"
+
+# add the README.md file to the git repository
+git -C "$PROJECT_LOCAL_LOCATION_PATH" add "./README.md"
+
+# commit the README.md file to the git repository
+git -C "$PROJECT_LOCAL_LOCATION_PATH" commit -m "Initial commit"
